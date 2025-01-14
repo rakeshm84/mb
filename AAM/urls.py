674424c608eb5d5d4619d4278fdf6a9d61c2c9f1
Rakesh Kumar <san888web@gmail.com>
2025-01-14 13:33:47 +0530

    Updated test url

--- AAM/urls.py
+++ AAM/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import TestView
 
 urlpatterns = [
-    path('test', TestView.as_view(), name='test'),
+    path('test/', TestView.as_view(), name='test'),
 ]
