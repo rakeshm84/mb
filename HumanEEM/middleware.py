4ac7d941f1bfab24b68d218d37c7ead716de7130
Rakesh Kumar <san888web@gmail.com>
2025-01-22 16:31:02 +0530

    human middleware

--- HumanEEM/middleware.py
+++ HumanEEM/middleware.py
@@ -8,19 +8,20 @@ class DatabaseMiddleware:
     def __call__(self, request):
 
         auth_header = request.headers.get('Authorization')
-        token = auth_header.split(' ')[1]
-        # token = request.COOKIES.get('refresh_token')
-        if token:
-            try:
-                decoded_token = AccessToken(token)
-                dsn = decoded_token.payload.get('dsn')
-                # print(f"Decoded Token: {dsn}")
-                if dsn:
-                    make_db_connection(dsn)
-                else:
-                    print("Error: DSN not found in token payload")
-            except Exception as e:
-                print(f"Error decoding token: {e}")
+        if auth_header:
+            token = auth_header.split(' ')[1]
+            # token = request.COOKIES.get('refresh_token')
+            if token:
+                try:
+                    decoded_token = AccessToken(token)
+                    dsn = decoded_token.payload.get('dsn')
+                    # print(f"Decoded Token: {dsn}")
+                    if dsn:
+                        make_db_connection(dsn)
+                    else:
+                        print("Error: DSN not found in token payload")
+                except Exception as e:
+                    print(f"Error decoding token: {e}")
 
         response = self.get_response(request)
         return response
