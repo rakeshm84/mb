0eef508aea7d7b86b0b1a1511b55e87c26d1c3bb
Rakesh Kumar <san888web@gmail.com>
2025-01-22 17:38:48 +0530

    Update base.py

--- mb/settings/base.py
+++ mb/settings/base.py
@@ -193,7 +193,7 @@ CORS_ALLOW_HEADERS = [
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
